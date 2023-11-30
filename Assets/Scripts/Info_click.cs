e626d6345a8f7890fccb43a91d2ce0951737f4dd
EdoardoEula <115698580+EdoardoEula@users.noreply.github.com>
2023-11-30 20:46:35 +0100

    Info Script

--- Assets/Scripts/Info_click.cs
+++ Assets/Scripts/Info_click.cs
@@ -3,46 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
 
 public class Info_click : MonoBehaviour
 {
     public Button myButton;
-    public TextMeshProUGUI info_title;   // Reference to the Text component for drag and drop
+    public TextMeshProUGUI info_title;
     public TextMeshProUGUI titletocopy;
     public TextMeshProUGUI info_description;
-    public string description_input;
-    public GameObject infoPanel; // Reference to the info_panel GameObject
+    public GameObject infoPanel;
     private bool isPanelOpen = false;
     public Transform mytransform;
-        
-    
-    // Start is called before the first frame update
+
+    private DatabaseReference databaseReference;
+
     void Start()
     {
         myButton.onClick.AddListener(ToggleVisibility);
+
+        // Initialize Firebase
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            FirebaseApp app = FirebaseApp.DefaultInstance;
+            databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+        });
     }
 
-    // Update is called once per frame
     void ToggleVisibility()
     {
-        // Check if the panel is currently open
         if (isPanelOpen)
         {
-            // If open, close the panel and update the flag
             infoPanel.SetActive(false);
             isPanelOpen = false;
         }
         else
         {
-            // If closed, open the panel and update the flag
             infoPanel.SetActive(true);
             isPanelOpen = true;
 
             // Set the position of the info_panel using the transform's position
-            // You can modify the position values based on your specific requirements
             infoPanel.transform.position = mytransform.position;
-            info_title.text = titletocopy.text;
-            info_description.text = description_input;
+
+            // Get the choice selected
+            string selectedChoice = titletocopy.text;
+
+            // Query the Firebase database for the selected choice
+            QueryDatabase(selectedChoice);
         }
+    }
+
+    void QueryDatabase(string selectedChoice)
+    {
+        // Reference to the "infos" node in the database
+        DatabaseReference infosRef = databaseReference.Child("infos");
+
+        infosRef.OrderByChild("Choice").EqualTo(selectedChoice).GetValueAsync().ContinueWithOnMainThread(task => {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Error querying database: " + task.Exception);
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+
+                // Iterate through the results (there should be only one match)
+                foreach (DataSnapshot infoSnapshot in snapshot.Children)
+                {
+                    // Extract the "Info" field and set it to info_description
+                    string infoText = infoSnapshot.Child("Info").Value.ToString();
+                    info_title.text = selectedChoice;
+                    info_description.text = infoText;
+                }
+            }
+        });
     }
 }
