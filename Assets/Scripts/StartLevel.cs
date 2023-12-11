5d07ee59a56225aa26a2559d6d4ee0bd0ff85492
EdoardoEula <115698580+EdoardoEula@users.noreply.github.com>
2023-12-11 14:06:22 +0100

    Solved an error in startLevel script

--- Assets/Scripts/StartLevel.cs
+++ Assets/Scripts/StartLevel.cs
@@ -2,19 +2,10 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.Netcode;
 public class StartLevel : MonoBehaviour
 {
     public TMP_Text moneyText;
-    
-    private Dictionary<string, float> educationMapping = new Dictionary<string, float>
-    {
-        {"Elementary School", 5.0f},
-        {"Middle School", 8.0f},
-        {"High School", 13.0f},
-        {"Bachelor's Degree", 18.0f},
-        {"Master's Degree", 22.0f},
-        {"Doctoral Degree", 25.0f}
-    };
 
     void Start()
     {
@@ -31,12 +22,5 @@ public class StartLevel : MonoBehaviour
 
     private void Update()
     {
-        string userGender = GameManager.gender;
-        int gender_Female = (userGender == "Male") ? 1 : 0;
-        int gender_Male = (userGender == "Female") ? 1 : 0;
-        int gender_Nonbinary = (userGender == "Non-binary") ? 1 : 0;
-        int gender_Prefernottosay = (userGender == "Prefer not to say") ? 1 : 0;
-        
-        educationMapping.TryGetValue(GameManager.education, out float education);
     }
 }
